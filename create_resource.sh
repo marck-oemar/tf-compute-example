6bbc03d1a27a1065cf3e0ebd7a644aef944bdcf7
Marck Oemar <marck.oemar@gmail.com>
2021-08-24 11:14:44 +0000

    move and improve

--- create_resource.sh
+++ create_resource.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x 
 
 echo "generating random resource id"
 RESOURCE_ID=$(tr -dc a-z0-9 </dev/urandom | head -c 6)
@@ -7,9 +8,16 @@ echo "resource id: ${RESOURCE_ID}"
 TERRAFORM_WORKSPACE=${RESOURCE_ID}
 TFENV=${RESOURCE_ID}
 
+# allow for concurrent tf run with temp directory
+echo "create temp. working directory, copy current working directory and traverse to temp. working directory"
+CWD=$(pwd)
+TMP_CWD=$(mktemp -d "${TMPDIR:-/tmp}/cwd-XXXXXXXXXX")
+cp -r . ${TMP_CWD}
+cd ${TMP_CWD}/tf
+
 terraform init
 
-if [[ $(terraform workspace list | grep "\s${TERRAFORM_WORKSPACE}$") ]]; then
+if [ $(terraform workspace list | grep "\s${TERRAFORM_WORKSPACE}$") ]; then
   echo "Selecting the workspace..."
   terraform workspace select ${TERRAFORM_WORKSPACE}
   if ! [ -z $(terraform state list) ] ; then echo "this terraform workspace already contains infra resources. Cannot create already existing resource" ; exit 1 ; fi
@@ -19,3 +27,8 @@ else
 fi
 
 TF_VAR_env="${TFENV}" terraform apply -auto-approve
+
+# allow for concurrent tf run with temp directory
+echo "traverse to starting working directory and delete temp. working directory"
+cd ${CWD}
+rm -rf ${TMP_CWD}
---
+++ delete_resource.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -x 
+
+if [ -z "$1" ] ; then echo "no resource id given" ; exit 1 ; fi 
+RESOURCE_ID="$1"
+TERRAFORM_WORKSPACE=${RESOURCE_ID}
+TFENV=${RESOURCE_ID}
+
+# allow for concurrent tf run with temp directory
+echo "create temp. working directory, copy current working directory and traverse to temp. working directory"
+CWD=$(pwd)
+TMP_CWD=$(mktemp -d "${TMPDIR:-/tmp}/cwd-XXXXXXXXXX")
+cp -r . ${TMP_CWD}
+cd ${TMP_CWD}/tf
+
+terraform init
+
+if [ "$(terraform workspace list | grep "\s${TERRAFORM_WORKSPACE}$")" ]; then
+  echo "Selecting the workspace..."
+  terraform workspace select ${TERRAFORM_WORKSPACE}
+else
+  echo "Workspace based on stack id $1 does not exist, cannot delete stack"
+  exit 1
+fi
+
+TF_VAR_env="${TFENV}" terraform destroy -auto-approve
+terraform workspace select default
+terraform workspace delete ${TERRAFORM_WORKSPACE}
+
+# allow for concurrent tf run with temp directory
+echo "traverse to starting working directory and delete temp. working directory"
+cd ${CWD}
+rm -rf ${TMP_CWD}
