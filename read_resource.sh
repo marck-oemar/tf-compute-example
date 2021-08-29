1eecb1263f9b17f0bce11d607bbca4ce5a24ebd5
Marck Oemar <marck.oemar@gmail.com>
2021-08-29 21:27:25 +0000

    add read

---
+++ read_resource.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -x 
+
+if [ -z "$1" ] ; then echo "no resource id given" ; exit 1 ; fi 
+RESOURCE_ID="$1"
+TERRAFORM_WORKSPACE=${RESOURCE_ID}
+TFENV=${RESOURCE_ID}
+
+# allow for concurrent tf run with temp directory
+echo "create temp. working directory, copy current working directory and traverse to temp. working directory"
+CWD=$(pwd)
+TMP_CWD=$(mktemp -d "${TMPDIR:-/tmp}/cwd-XXXXXXXXXX")
+cp -r . ${TMP_CWD}
+cd ${TMP_CWD}/tf
+
+terraform init
+
+if [ "$(terraform workspace list | grep "\s${TERRAFORM_WORKSPACE}$")" ]; then
+  echo "Selecting the workspace..."
+  terraform workspace select ${TERRAFORM_WORKSPACE}
+else
+  echo "Workspace based on stack id $1 does not exist, cannot continue"
+  exit 1
+fi
+
+list=$(terraform state list)
+for i in ${list}; do
+  echo ""
+  echo "=========== $i ============"
+  terraform state show $i
+done
+terraform workspace select default
+
+# allow for concurrent tf run with temp directory
+echo "traverse to starting working directory and delete temp. working directory"
+cd ${CWD}
+rm -rf ${TMP_CWD}
